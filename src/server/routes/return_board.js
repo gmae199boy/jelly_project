1c9fe30fe6a47949fc1eefead580693e9e728aef
gmae199boy <kim88594544@gmail.com>
2020-06-30 22:49:00 +0900

    commit

--- src/server/routes/return_board.js
+++
@@ -1,9 +0,0 @@
-module.exports.return_board = function(req, res){
-    var db = req.app.get('db');
-    db.DrawingBoardModel.find({}, function(err, result){
-        if(err) throw err;
-        if(result){
-            res.status(200).send(result);
-        }
-    });
-};
