bb8e600a36a571204da7d3c68c1b467e980584a4
gmae199boy <kim88594544@gmail.com>
2020-07-03 16:22:44 +0900

    commit

--- truffle-config.js
+++ truffle-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
+      host: "0.0.0.0",
+      port: 5545,
       network_id: "*" // Match any network id
     }
   },
