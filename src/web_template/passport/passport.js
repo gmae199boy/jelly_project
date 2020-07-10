0ceb97f6bdfe5f4b7bf936a3a42a692b44e353bf
gmae199boy <kim88594544@gmail.com>
2020-07-10 15:26:04 +0900

    changed regist

--- src/web_template/passport/passport.js
+++
@@ -1,6 +0,0 @@
-var passport = require('passport');
-var User = require('./models/user');
-
-passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
