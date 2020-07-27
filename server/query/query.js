7ad1529434e8dd9d1d0e6f00050d02af0d6f44fc
gmae199boy <kim88594544@gmail.com>
2020-07-27 09:35:57 +0900

    DB 쿼리를 분리

---
+++ server/query/query.js
@@ -0,0 +1,49 @@
+var User = require("../model/user");
+var Product = require("../model/product");
+
+var query = {};
+
+//한 페이지에 20개의 펀딩까지 보여준다.
+//readProductList
+const perPage = 20;
+
+// 유저가 참여한 product의 목록을 배열로 리턴한다.
+query.readUserProducts = function(userId, callback) {
+    User.find({userId: userId}, {myProducts: 1})
+    .populate('myProducts')
+    .exec((err, result) => {
+        if (err) {callback(err, null); return;}
+        callback(null, result);
+        return;
+    });
+}
+
+// 현재 진행중인 productList를 배열로 반환한다.
+query.readProductList = function(page, status = 2, callback) {
+    //status = 1 : 펀딩 진행 전, status = 2 : 펀딩 진행 중, status = 4 : 펀딩 종료
+    Product.find({status: status}, { _id: 0, productId: 1, name: 1, type: 1, amount: 1, startDate: 1, endDate: 1, desc: 1, status: 1 })
+    .sort({ $natural: 1 })
+    .skip(page * perPage)
+    .limit(perPage)
+    .lean()
+    .exec((err, result) => {
+        if (err) {callback(err, null); return;}
+        if (result) { // 전송 할 데이터가 있으면
+            callback(null, result);
+            return;
+        } else {
+            callback(null, null);
+            return;
+        }
+    });
+}
+
+query.readProduct = function(productId, callback) {
+    Product.findOne({productId: productId}).exec((err, result) => {
+        if (err) {callback(err, null); return;}
+        callback(null, result);
+        return;
+    });
+}
+
+module.exports = query;
