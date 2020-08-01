7c435e704185c3792481da6b0f3e448ff5b33cf6
gmae199boy <kim88594544@gmail.com>
2020-08-01 16:27:16 +0900

    유저타입에 따른 표시정보 변경

--- server/router/mypage.js
+++ server/router/mypage.js
@@ -7,18 +7,34 @@ passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+const DONOR = 0;
+const RECIPIENT = 1;
+
 
 module.exports = function(contract, account){
     /* GET home page. */
     router.get('/', function(req, res, next) {
-        queryPromise.getPopulatedUserForMyFundingList(req.user.userId).then((result) => {
-            console.log(result);
-            console.log(result.myFundingList[0].id);
-            req.user = result;
-            res.render('mypage', {
-                user: req.user,
-            })
-        });
+        switch(req.user.userType) {
+            case DONOR: {
+                queryPromise.getPopulatedUserForMyFundingList(req.user.userId).then((result) => {
+                    console.log(result);
+                    console.log(result.myFundingList[0].id);
+                    req.user = result;
+                    res.render('mypage', {
+                        user: req.user,
+                    })
+                });
+            }
+            break;
+            case RECIPIENT: {
+
+            }
+            break;
+            default: {
+                console.log("유저 타입이 적절하지 않다");
+                res.send("유저 타입이 적절하지 않다.");
+            }
+        }
     });
     return router;
 }
